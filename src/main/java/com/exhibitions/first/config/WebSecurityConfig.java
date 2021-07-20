11de77adac3c2ea565f7688b92084ad43e23e187
Adramelech9 <lapitan31343@gmail.com>
2021-07-20 23:17:41 +0300

    my

--- src/main/java/com/exhibitions/first/config/WebSecurityConfig.java
+++ src/main/java/com/exhibitions/first/config/WebSecurityConfig.java
@@ -18,6 +18,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().disable()
+                .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/", "/post", "/post/{id}", "/registration", "/filter").permitAll()
                     .anyRequest().authenticated()
